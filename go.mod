2009aae5e2fa47fa13360d6db8b7113ab1c125b7
Sergey Chumbler <schumbler@projectx019.com>
2019-12-10 10:59:06 +0300

    add pack

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/SergeyChumbler/go-mod-version
+
+go 1.13
+
+require rsc.io/quote v1.5.2
---
+++ hello.go
@@ -0,0 +1,8 @@
+package hello
+
+import "rsc.io/quote"
+
+// Hello ...
+func Hello() string {
+    return quote.Hello()
+}
